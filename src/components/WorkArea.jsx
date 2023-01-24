8e5c602dcbb1075fb32d45ee41bc3c128bb2aa09
Antreas Papadopoulos <antreas.paps@yahoo.com>
2023-01-24 01:07:44 +0000

    First Commit

---
+++ src/components/WorkArea.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Loader() {
+   return (
+    <div className="loader" id="loader" class="fixed top-0 left-0 grid place-items-center w-full h-full">
+    <div className=''>Do stuff</div>
+  </div>
+   )
+}
+
+export default Loader;
---
+++ tailwind.config.cjs
@@ -0,0 +1,37 @@
+module.exports = {
+	content: ['./index.html', './src/**/*.{jsx, js}'],
+	darkMode: 'class',
+	theme: {
+		extend: {
+			fontFamily: {
+				inter: ['inter', 'serif'],
+			},
+			keyframes: {
+				rotation: {
+					'0%': { rotate: '0deg'},
+					'100%': { rotate: '360deg'},
+				},
+				rotationRev: {
+					'0%': { rotate: '0deg'},
+					'100%': { rotate: '-360deg'},
+				},
+				bounce: {
+					'0%': { transform: 'translateY(0)' },
+					'50%': { transform: 'translateY(-20px)' },
+					'100%': { transform: 'translateY(0)' }
+				},
+			},
+			animation: {
+				'rotate': 'rotation 1.5s linear infinite',
+				'point': 'bounce 1s infinite ease-in-out',
+			},
+		},
+		screens: {
+			'sm': '640px',
+			'md': '768px',
+			'lg': '1024px',
+			'xl': '1280px',
+		}
+	},
+	plugins: [],
+};
